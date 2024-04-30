38c9972421c484b0f47862ebdc0a660b4d8777fe
DGHaZe <34streets@gmail.com>
2024-04-30 20:07:22 +0300

    nead fineshing

---
+++ c_dz_3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Ваш вік:" << " "<< "Тип int";
+    cout << "Площа вашого заднього двору." << " "<< "Тип float или double";
+    cout << "Кількість зірок у галактиці." << " "<< "";
+    cout << "Середній рівень випадання опадів за січень." << " "<< "";
+
+    return 0;
+}
